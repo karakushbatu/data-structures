9043a8d6cdc5ab1443bdf33244bde487ce8ad0fe
Synthethic <114388464+karakushbatu@users.noreply.github.com>
2025-03-25 23:39:03 +0300

    Create bfs.cpp

---
+++ graphs/bfs.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(int start, vector<vector<int>>& adj) {
+    vector<bool> visited(adj.size(), false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int V = 5;
+    vector<vector<int>> adj(V);
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0};
+    adj[3] = {1, 4};
+    adj[4] = {3};
+
+    cout << "BFS traversal: ";
+    bfs(0, adj);
+    cout << endl;
+
+    return 0;
+}
