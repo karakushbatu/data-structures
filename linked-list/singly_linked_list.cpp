695015d07803b044e6fdf411bd1149a270d1ae50
Synthethic <114388464+karakushbatu@users.noreply.github.com>
2025-03-25 23:15:10 +0300

    Update singly_linked_list.cpp

--- linked-list/singly_linked_list.cpp
+++ linked-list/singly_linked_list.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+using namespace std;
 
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+public:
+    SinglyLinkedList() : head(nullptr) {}
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* curr = head;
+        while (curr->next)
+            curr = curr->next;
+        curr->next = newNode;
+    }
+
+    void display() {
+        Node* curr = head;
+        while (curr) {
+            cout << curr->data << " -> ";
+            curr = curr->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    ~SinglyLinkedList() {
+        Node* curr = head;
+        while (curr) {
+            Node* temp = curr;
+            curr = curr->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.display();
+    return 0;
+}
